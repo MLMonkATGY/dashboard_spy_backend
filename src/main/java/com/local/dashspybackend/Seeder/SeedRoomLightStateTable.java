9b06e87e1f87af66d7a34fc9b7e9ea7bbd7a8b65
alextaygeeyang <alextaygeeyang96@gmail.com>
2020-10-24 18:19:05 +0800

    Updated seeder for default device name

--- src/main/java/com/local/dashspybackend/Seeder/SeedRoomLightStateTable.java
+++ src/main/java/com/local/dashspybackend/Seeder/SeedRoomLightStateTable.java
@@ -12,35 +12,34 @@ import com.local.dashspybackend.Entity.DeviceInfoEntity;
 import com.local.dashspybackend.Entity.LightStateEntity;
 import com.local.dashspybackend.Repository.IDeviceInfoRepo;
 import com.local.dashspybackend.Repository.ILightStateRepo;
+
 @Component
 public class SeedRoomLightStateTable {
     @Autowired
     private IDeviceInfoRepo repo;
     @Autowired
     private ILightStateRepo lightRepo;
+
     @EventListener
-    public void seed(ContextRefreshedEvent event){
+    public void seed(ContextRefreshedEvent event) {
         seedDevice();
-       
 
     }
-    public void seedDevice(){
-        if(repo.count() == 0){
-            DeviceInfoEntity d1 = 
-            new DeviceInfoEntity();
+
+    public void seedDevice() {
+        if (repo.count() == 0) {
+            DeviceInfoEntity d1 = new DeviceInfoEntity();
             d1.setApiKey("f6b401fb-028d-4009-ab16-eec64e529b16");
-            
 
             d1.setDeviceId("10006c0aff");
-            d1.setDeviceName("light switch");
+            d1.setDeviceName("laptop_charger");
             d1.setLocalAddress("192.168.1.15");
             d1.setInitializationVectorOn("XhWfC4JmNbovXdQ4IoJqSw==");
             d1.setEncryptedPayloadOn("atwd4pAfXVAwcmsht0isEQ==");
             d1.setInitializationVectorOff("OehnhgrhYVpwTZBquvf8Ug==");
             d1.setEncryptedPayloadOff("x0jSlXZnHz8a4fFlegAtHHHPcHv66/RX8xmw8JMi9mw=");
             d1.setSelfApi("123");
-            DeviceInfoEntity d2 = 
-            new DeviceInfoEntity();
+            DeviceInfoEntity d2 = new DeviceInfoEntity();
             d2.setApiKey("6a55cae4-ba5d-4377-b941-247eb2036477");
             d2.setDeviceId("100073bfba");
             d2.setDeviceName("room_light");
@@ -57,24 +56,25 @@ public class SeedRoomLightStateTable {
         }
 
     }
-    public void generateSeedData(int seedNum, String deviceId){
-        List<DeviceInfoEntity> devices =  repo.findAll();
-        
+
+    public void generateSeedData(int seedNum, String deviceId) {
+        List<DeviceInfoEntity> devices = repo.findAll();
+
         for (DeviceInfoEntity device : devices) {
             List<LightStateEntity> a = new ArrayList<>();
 
-            for(int i = 0 ; i< seedNum; i++){
+            for (int i = 0; i < seedNum; i++) {
                 LightStateEntity mockData = new LightStateEntity();
                 mockData.setSeq(i);
-                mockData.setLightState(Math.random()>0.5);
+                mockData.setLightState(Math.random() > 0.5);
                 mockData.setDevice(device);
                 a.add(mockData);
-               
-            }   
+
+            }
 
             device.setStates(a);
             repo.save(device);
         }
-        
+
     }
 }
